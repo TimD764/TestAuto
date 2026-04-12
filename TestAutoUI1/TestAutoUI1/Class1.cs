f8ce14ac0514a6e6fdabe2b57e616b6f88b17944
tim <timofficialfacs@gmail.com>
2026-04-12 12:25:33 +0300

    Add project files.

---
+++ TestAutoUI1/TestAutoUI1/Class1.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace EhuAutomation;
+
+[TestFixture]
+public class EhuTests
+{
+    private IWebDriver _driver;
+    private WebDriverWait _wait;
+
+    [SetUp]
+    public void Setup()
+    {
+        _driver = new ChromeDriver();
+        _driver.Manage().Window.Maximize();
+        _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+    }
+    private IWebElement WaitForVisible(By locator)
+    {
+        return _wait.Until(d =>
+        {
+            var element = d.FindElements(locator).FirstOrDefault();
+            return (element != null && element.Displayed) ? element : null;
+        });
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _driver.Quit();
+        _driver.Dispose();
+    }
+
+    [Test]
+    public void VerifyAboutPage_Test1()
+    {
+        _driver.Navigate().GoToUrl("https://en.ehu.lt/");
+
+        // Wait for the 'About' link to be clickable, then click it
+        var aboutLink = _wait.Until(d => d.FindElement(By.LinkText("About")));
+        aboutLink.Click();
+
+        // Wait for the URL to change
+        _wait.Until(d => d.Url.Contains("about/"));
+
+        // Assertions
+        Assert.That(_driver.Url, Does.Contain("about/"));
+        Assert.That(_driver.Title, Does.Contain("About"));
+    }
+
+    [Test]
+    public void VerifySearch_Test2()
+    {
+        _driver.Navigate().GoToUrl("https://en.ehuniversity.lt/?s=study+programs");
+
+        _wait.Until(d => d.Url.Contains("?s=study+programs"));
+
+        Assert.That(_driver.Url, Does.Contain("?s=study+programs"),
+            "URL did not contain queri");
+
+        var resultLinks = _driver.FindElements(By.CssSelector("a[href*='/studies/']"));
+        Assert.That(resultLinks.Count, Is.GreaterThan(0),
+            "No links to study programs.");
+    }
+
+    [Test]
+    public void VerifyLanguageChange_Test3()
+    {
+        _driver.Navigate().GoToUrl("https://en.ehuniversity.lt/");
+
+        try
+        {
+            var cookies = _wait.Until(d => d.FindElement(By.XPath("//button[contains(., 'I agree') or contains(., 'Accept')]")));
+            cookies.Click();
+        }
+        catch
+        {
+
+        }
+
+        var ltButton = _wait.Until(d => d.FindElement(By.XPath("//a[text()='LT' or text()='lt' or contains(@href, 'lt.')]")));
+
+        ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", ltButton);
+
+        _wait.Until(d => d.Url.Contains("lt."));
+
+        Assert.That(_driver.Url, Does.Contain("lt."), "URL did not change to lithuanian subdomain.");
+
+        var bodyText = _driver.FindElement(By.TagName("body")).Text;
+        Assert.That(bodyText, Does.Match("Apie|Kontaktai|Pradžia"), "No lithuanian text");
+    }
+
+    [Test]
+    public void VerifyContacts_Test4()
+    {
+        _driver.Navigate().GoToUrl("https://en.ehuniversity.lt/contact/");
+
+        var bodyText = WaitForVisible(By.TagName("body")).Text;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(bodyText, Does.Contain("franciskscarynacr@gmail.com"), "Email not found");
+            Assert.That(bodyText, Does.Contain("+370 68 771365"), "There is no lithuanian number");
+            Assert.That(bodyText, Does.Contain("+375 29 5781488"), "There is no Balarusian number");
+            Assert.That(bodyText, Does.Contain("Facebook"), "No facebook link");
+            Assert.That(bodyText, Does.Contain("Telegram"), "No Telegram link");
+            Assert.That(bodyText, Does.Contain("VK"), "No vkontakte link");
+        });
+    }
+}
